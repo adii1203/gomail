c1c403d34992173928f5a10052d8da3077662dd3
adii1203 <aditya32ft@gmail.com>
2024-12-09 23:39:50 +0530

    Add email format validation.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"regexp"
+	"strings"
+)
 
-func main(){
-	fmt.Println("Hello World")
+func main() {
+}
+
+func verifyEmailFormat(email string) (bool, string) {
+	emailRegex := "^(((([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|\\.|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.?$"
+	email = strings.TrimSpace(email)
+	// email length < 320.
+	if len(email) > 320 {
+		return false, "Email is to long."
+	}
+
+	parts := strings.Split(email, "@")
+	if len(parts) != 2 {
+		return false, "Email must contain exactly one '@'"
+	}
+
+	userPart, domainPart := parts[0], parts[1]
+	userPartLength := len(userPart)
+	domainPartLength := len(domainPart)
+
+	if userPartLength > 64 {
+		return false, "User part exceeds 64 characters"
+	}
+
+	if domainPartLength > 253 {
+		return false, "Domain part exceeds 253 characters"
+	}
+
+	match, err := regexp.MatchString(emailRegex, email)
+	if err != nil || !match {
+		return false, "Invalid email address."
+	}
+
+	return true, "Valid email address."
 }
